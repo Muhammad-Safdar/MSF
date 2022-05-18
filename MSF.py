5d8c6b10674a73b7234a018f7a0258c04ff54bf0
Muhammad-Safdar <105332053+Muhammad-Safdar@users.noreply.github.com>
2022-05-18 20:49:55 +0500

    Update MSF.py

--- MSF.py
+++ MSF.py
@@ -8,7 +8,7 @@ import requests
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
-    print("\033[1;37mOops sorry brother Tool has been locked")
+    print("\033[1;31mOops sorry brother Tool has been locked")
 elif bit == '32bit':
     from crac9 import __xXx____
     __xXx____()
